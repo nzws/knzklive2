447e37b796236ac907e46bfdee20af7f5e86b56d
Yuzuki Aida <dev@nzws.me>
2022-08-22 01:39:55 +0900

    a

--- web/next.config.js
+++ web/next.config.js
@@ -5,23 +5,30 @@ const withBundleAnalyzer = nextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true'
 });
 
+const injectRule = rule => {
+  if (rule?.use?.loader !== 'next-swc-loader') {
+    return;
+  }
+
+  const injectPath = path.resolve(__dirname, '../api-types');
+  if (!rule.include.includes(injectPath)) {
+    rule.include.push(injectPath);
+    console.log('rule detected', rule);
+  }
+};
+
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
   webpack: config => {
     //console.dir(config.module.rules, { depth: null });
     for (const rule of config.module.rules) {
-      if (!rule.oneOf) {
-        continue;
-      }
-
-      for (const loader of rule.oneOf) {
-        if (!(loader.test instanceof RegExp)) {
-          continue;
+      if (rule.oneOf) {
+        for (const subRule of rule.oneOf) {
+          injectRule(subRule);
         }
-        if (loader.test.toString() === '/\\.(tsx|ts|js|cjs|mjs|jsx)$/') {
-          loader.include.push(path.resolve(__dirname, '../api-types'));
-        }
+      } else {
+        injectRule(rule);
       }
     }
     return config;
