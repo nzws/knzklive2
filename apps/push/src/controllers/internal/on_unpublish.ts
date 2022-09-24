90536708241b474d3bf3744c4fd667f97e387255
Yuzuki Aida <dev@nzws.me>
2022-09-24 17:09:49 +0900

    Remove check token in unpublish

--- apps/push/src/controllers/internal/on_unpublish.ts
+++ apps/push/src/controllers/internal/on_unpublish.ts
@@ -1,5 +1,4 @@
 import { Middleware } from 'koa';
-import { backendJwt } from '../../services/jwt';
 import { SRSUnPublishCallback } from '../../types';
 import { client, serverToken } from '../../utils/api';
 import { rejectSession } from '../../utils/sessions';
@@ -17,22 +16,11 @@ export const apiInternalOnUnPublish: Middleware = async ctx => {
   }
 
   const liveId = parseInt(body.stream.split('_')[0], 10);
-  const token = body.param.replace('?token=', '');
-  if (!liveId || !token) {
+  if (!liveId) {
     ctx.status = 400;
     ctx.body = {
       code: 400,
       message: 'Invalid liveId or token'
-    };
-    return;
-  }
-
-  const verify = await backendJwt.check(token, liveId, 'push');
-  if (!verify) {
-    ctx.status = 401;
-    ctx.body = {
-      code: 401,
-      message: 'Invalid token'
     };
     return;
   }
