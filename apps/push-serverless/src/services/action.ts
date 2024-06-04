75c0acc2a11e57b4e2f90a878b7aac5eca67b6f1
Yuzuki Aida <dev@nzws.me>
2024-06-05 01:21:53 +0900

    Remove recoding implement

--- apps/push-serverless/src/services/action.ts
+++ apps/push-serverless/src/services/action.ts
@@ -1,9 +1,6 @@
 import { lives, rejectSession, sessions } from '../utils/sessions';
 import { Encoder } from './encoder';
 import { generateToken } from '../utils/token';
-import { client, serverToken } from '../utils/api';
-
-const RECORDING_MAX_SECONDS = 60 * 60; // 1 hour
 
 export class Action {
   static async startStream(
@@ -38,16 +35,6 @@ export class Action {
   static async stopStream(liveId: number) {
     await rejectSession(liveId);
     sessions.delete(liveId);
-
-    const live = lives.get(liveId);
-    if (live?.lastRecordStartAt) {
-      const diff = Math.floor((Date.now() - live.lastRecordStartAt) / 1000);
-      lives.set(liveId, {
-        ...live,
-        recordingSeconds: live.recordingSeconds + diff,
-        lastRecordStartAt: undefined
-      });
-    }
   }
 
   static startLive(liveId: number, isRecording: boolean) {
@@ -55,10 +42,6 @@ export class Action {
       isRecording: isRecording,
       recordingSeconds: 0
     });
-
-    if (isRecording) {
-      // this.startRecording(liveId);
-    }
   }
 
   static async endLive(liveId: number) {
@@ -67,87 +50,7 @@ export class Action {
       lives.delete(liveId);
       return;
     }
-
-    void (async () => {
-      try {
-        const live = lives.get(liveId);
-        if (!live?.isRecording) {
-          lives.delete(liveId);
-          sessions.delete(liveId);
-          return;
-        }
-
-        void client.v1.internals.push.action.$post({
-          body: {
-            liveId,
-            action: 'record:processing',
-            serverToken
-          }
-        });
-
-        // 録画データの生成を待つ
-        await session.encoder.cleanup();
-
-        throw new Error('Not implemented');
-
-        /*
-
-        const path = await session.encoder.mergeRecording();
-        if (!path) {
-          throw new Error('merge recording failed');
-        }
-        const { size } = await stat(path, { bigint: true });
-        const { key } = await new VideoStorageClient().putMp4(liveId, path);
-        await session.encoder.cleanupMergedRecording();
-
-        void client.v1.internals.push.action.$post({
-          body: {
-            liveId,
-            action: 'record:done',
-            serverToken,
-            recordingKey: key,
-            fileSize: size.toString()
-          }
-        });
-        */
-      } catch (e) {
-        console.warn('merge recording failed', e);
-
-        void client.v1.internals.push.action.$post({
-          body: {
-            liveId,
-            action: 'record:failed',
-            serverToken
-          }
-        });
-      } finally {
-        sessions.delete(liveId);
-        lives.delete(liveId);
-      }
-    })();
-  }
-
-  private static startRecording(liveId: number) {
-    const prev = lives.get(liveId);
-    if (!prev?.isRecording) {
-      return;
-    }
-    if (prev && prev?.recordingSeconds >= RECORDING_MAX_SECONDS) {
-      console.warn('recording max seconds reached', liveId);
-      return;
-    }
-
-    const encoder = sessions.get(liveId)?.encoder;
-    const recordingSeconds = prev?.recordingSeconds ?? 0;
-    const remainingSeconds = RECORDING_MAX_SECONDS - recordingSeconds;
-    void encoder?.encodeToRecording(remainingSeconds);
-
-    if (!prev?.lastRecordStartAt) {
-      lives.set(liveId, {
-        isRecording: true,
-        recordingSeconds,
-        lastRecordStartAt: Date.now()
-      });
-    }
+    lives.delete(liveId);
+    sessions.delete(liveId);
   }
 }
